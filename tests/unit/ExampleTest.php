ee5718b0b8b930268ce2f09ef4099c41b3674170
Vladislav Seletskiy <vselecky@gmail.com>
2019-07-26 12:31:37 +0300

    Add phpunit

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample() : void
+    {
+        $this->assertTrue(true);
+    }
+}
